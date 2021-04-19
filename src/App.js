4451bdff1f4b96dee67faa65fc971b2cb09aff8c
MBesednik <m.besednik69@gmail.com>
2021-04-19 03:05:49 +0200

    added Link in Header component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
 import Home from './Pages/Home/Home';
 import Footer from './Components/Footer/Footer';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header/>
       <Main>
-       <Home/>
+        <Route exact path="/" component={Home} />
       </Main>
       <Footer/>
     </>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import './Header.scss';
 import Logo from '../../assets/Images/logo.png';
+import { Link } from 'react-router-dom';
+import Home from '../../Pages/Home/Home';
+
+const links = {
+    home: 'Home',
+    events: 'Events',
+}
 
 const Header = () => {
     return (
 <header className="Header">
         <div className="Header-Inner">
-            <a href="index.html" className="Header-LogoContainer">
-                <img src={Logo} className="Header-Logo" alt=""/>
-            </a>
+            <Link className="Header-LogoContainer" to="/">
+                <img src={Logo} className="Header-Logo" alt="Logo"/>
+                </Link>
+        
             <div className="Header-NavHamburger">
                     <div className="Header-NavHamburgerLine"></div>
                     <div className="Header-NavHamburgerLine"></div>
                     <div className="Header-NavHamburgerLine"></div>
                 </div>
             <nav className="Header-Nav">
-                <a href="index.html" className="Header-NavItem">Home</a>
-                <a href="events.html" className="Header-NavItem">Event</a>
+                <Link className="Header-NavItem" to="/" >{links.home}</Link>
+                <Link className="Header-NavItem" to="/events" >{links.events}</Link>
             </nav>
         </div>
     </header>
